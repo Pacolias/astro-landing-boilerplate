ed7733ec366bc37f988ec2feaca9e6c99f75f2e6
Pacolias <pacomolinac2003@gmail.com>
2026-09-03 01:47:00 +0200

    feat: Update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Pacolias.github.io',
+  base: '/astro-landing-boilerplate',
+
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  devToolbar: {
+    enabled: false
   }
 });
